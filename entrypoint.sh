aefe226a0e2a9f369204ccb2145e1e8bef91e9cf
qqq-qqqq <32921374+qqq-qqqq@users.noreply.github.com>
2026-03-28 18:54:39 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,34 +3,22 @@
 crond -l 2
 
 CRONJOB_FILE=/config/cronjob.sh
-
-if [ -f "$CRONJOB_FILE" ]; then
-	echo "$CRONJOB_FILE exist"
-	chmod +x $CRONJOB_FILE
-	chmod 777 $CRONJOB_FILE
-else 
+if [ ! -f "$CRONJOB_FILE" ]; then
 	echo "$CRONJOB_FILE does not exist"
-	cp /cronjob.sh $CRONJOB_FILE
-	chmod +x $CRONJOB_FILE
-	chmod 777 $CRONJOB_FILE
+	cp /cronjob.sh "$CRONJOB_FILE"
 fi
+chmod 777 "$CRONJOB_FILE"
 
 CRON_FILE=/config/cron.txt
-
-if [ -f "$CRON_FILE" ]; then
-	. $CRON_FILE
-else
-	printf '0  0  *  *  *  /config/cronjob.sh' > /etc/crontabs/root
-	cp /sample_cron.txt /config/sample_cron.txt
+if [ ! -f "$CRON_FILE" ]; then
+	cp /sample_cron.txt "$CRON_FILE"
 fi
+. "$CRON_FILE"
 
 XTEVE_FILE=/config/xteve.txt
-
-if [ -f "$XTEVE_FILE" ]; then
-	. $XTEVE_FILE
-else
-	cp /sample_xteve.txt /config/sample_xteve.txt
-	xteve -port=34400 -config=/root/.xteve/
+if [ ! -f "$XTEVE_FILE" ]; then
+	cp /sample_xteve.txt "$XTEVE_FILE"
 fi
+. "$XTEVE_FILE"
 
 exit
